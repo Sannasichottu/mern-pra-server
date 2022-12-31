e138c77c06432d55eecc3a4c08b2efc7eb7fe798
Sannasi <sannasichottu@gmail.com>
2022-12-31 17:56:47 +0530

    first commit

---
+++ controllers/userCtrl.js
@@ -0,0 +1,64 @@
+const Users = require('../models/userModel');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+
+const {CLIENT_URL} = process.env
+
+const userCtrl = {
+    register: async(req,res) => {
+        try{
+            const {name, email, password} = req.body;
+
+            if(!name || !email || !password)
+            return res.status(400).json({msg: "please fill in all fields."})
+
+            if(!validateEmail(email))
+            return res.status(400).json({msg: "Invalid emails."})
+
+            const user = await Users.findOne({email})
+            if(user) return res.status(400).json({msg:"This email already exits."})
+
+            if(password.length < 6)
+                return res.status(400).json({msg:"Password must be at least 6 characters."})
+
+            const passwordHash = await bcrypt.hash(password, 12)
+            console.log({password, passwordHash});
+
+            const newUser = {
+                name, email, password : passwordHash
+            }
+
+            const activation_token = createActivationToken(newUser)
+
+            const url = `${CLIENT_URL}/user/activate/${activation_token}`
+
+            sendMail(email,url)
+
+
+            res.json({msg:"Register Success! Please activate your email to start."})
+        }catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    }
+}
+
+function validateEmail(email) {
+    const re =
+  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;;
+    return re.test(email);
+  }
+
+  const createActivationToken = (payload) => {
+    return jwt.sign(payload, process.env.ACTIVATION_TOKEN_SECRET, {expiresIn:'15m'} )
+  }
+
+  const createAccessToken = (payload) => {
+    return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'15m'} )
+  }
+
+  const createRefershToken = (payload) => {
+    return jwt.sign(payload, process.env.REFERSH_TOKEN_SECRET, {expiresIn:'7d'} )
+  }
+
+
+module.exports = userCtrl;
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+require('dotenv').config();
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use(fileUpload({
+    useTempFiles: true
+}))
+
+//Routes
+app.use('/user',require('./routes/userRouter'))
+
+
+
+/*
+app.use('/',(req,res,next) => {
+    res.json({msg: "Hello Nanba!"})
+})*/
+
+//connect to mongo
+const uri = process.env.DB_URI
+mongoose.set('strictQuery',false)
+mongoose.connect(uri,err=>{
+    if(err)throw err
+})
+
+const connection = mongoose.connection;
+
+connection.once('open',()=>{
+    console.log("Database connect successfully")
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log('Server is running on port',PORT)
+})
