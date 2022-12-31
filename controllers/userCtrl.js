64ed19368fc81a7b9d4a601697f3e0fd4b590576
Sannasi <sannasichottu@gmail.com>
2022-12-31 20:57:35 +0530

    login-user

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -61,7 +61,28 @@ const userCtrl = {
       } catch (err) {
         return res.status(500).json({msg:err.message})
       }
-    }
+    },
+    login: async (req, res) => {
+      try {
+          const {email, password} = req.body
+          const user = await Users.findOne({email})
+          if(!user) return res.status(400).json({msg: "This email does not exist."})
+
+          const isMatch = await bcrypt.compare(password, user.password)
+          if(!isMatch) return res.status(400).json({msg: "Password is incorrect."})
+
+          const refresh_token = createRefreshToken({id: user._id})
+          res.cookie('refreshtoken', refresh_token, {
+              httpOnly: true,
+              path: '/user/refresh_token',
+              maxAge: 7*24*60*60*1000 // 7 days
+          })
+
+          res.json({msg: "Login success!"})
+      } catch (err) {
+          return res.status(500).json({msg: err.message})
+      }
+  }
 }
 
 function validateEmail(email) {
@@ -78,8 +99,8 @@ function validateEmail(email) {
     return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'15m'} )
   }
 
-  const createRefershToken = (payload) => {
-    return jwt.sign(payload, process.env.REFERSH_TOKEN_SECRET, {expiresIn:'7d'} )
+  const createRefreshToken = (payload) => {
+    return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {expiresIn:'7d'} )
   }
 
 
